caf269bc50b27c9c41c4affc17158588a34e067e
Andrey <a.stegno@gmail.com>
2015-10-23 14:08:18 +0000

    Development

---
+++ index.js
@@ -0,0 +1,26 @@
+require('colors');
+var chai = require("chai");
+var chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+chai.should();
+
+var wd = require('wd');
+
+// enables chai assertion chaining
+chaiAsPromised.transferPromiseness = wd.transferPromiseness;
+
+var browser = wd.promiseChainRemote();
+
+// optional extra logging
+browser.on('status', function(info) {
+  console.log(info.cyan);
+});
+browser.on('command', function(eventType, command, response) {
+  console.log(' > ' + eventType.cyan, command, (response || '').grey);
+});
+browser.on('http', function(meth, path, data) {
+  console.log(' > ' + meth.magenta, path, (data || '').grey);
+});
+
+/* jshint evil: true */
+module.exports = browser;
